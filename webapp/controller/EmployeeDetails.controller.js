faa6cb00fca061d286999359417f18b9019b1f0d
angelogarrido <garridoangelosap@gmail.com>
2021-12-11 13:47:16 +0000

    bloque6 Lab5 2de4

--- webapp/controller/EmployeeDetails.controller.js
+++ webapp/controller/EmployeeDetails.controller.js
@@ -1,11 +1,33 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ], function (Controller) {
 
-    return Controller.extend("logaligroup.employees.controller.EmployeeDetails", {
+    function onInit() {
 
-        onInit: function () {
+    };
 
-        }
-    });
+
+    function onCreateIncidence() {
+
+        var tableIncidence = this.getView().byId("tableIncidence");
+        var newIncidence = sap.ui.xmlfragment("logaligroup.Employees.fragment.NewIncidence", this);
+        var incidenceModel = this.getView().getModel("incidenceModel");
+        var odata = incidenceModel.getData();
+        var index = odata.length;
+        odata.push({ index: index + 1 });
+        incidenceModel.refresh();
+        newIncidence.bindElement("incidenceModel>/" + index);
+        tableIncidence.addContent(newIncidence);
+
+    };
+
+    var EmployeeDetails = Controller.extend("logaligroup.employees.controller.EmployeeDetails", {});
+
+    EmployeeDetails.prototype.onInit = onInit;
+    EmployeeDetails.prototype.onCreateIncidence = onCreateIncidence;
+
+    return EmployeeDetails;
 }); 
+   
+
