1bdf6f8ddd28b45ea0f1c180a7f145cc1f6db5dd
Angelo Garrido <garridoangelosap@gmail.com>
2022-01-02 19:19:43 +0000

    8BloqueLab1Navegacion4de5

--- webapp/controller/OrderDetails.controller.js
+++ webapp/controller/OrderDetails.controller.js
@@ -1,12 +1,27 @@
 // @ts-nocheck
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
+], function (Controller, History) {
 
     return Controller.extend("logaligroup.employees.controller.OrderDetails", {
 
         onInit: function () {
 
+        },
+
+        onBack : function(oEvent) {
+
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("RouteMain", true);
+            }
         }
+
     });
 });     
